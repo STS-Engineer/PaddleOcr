64eec8db27af46a2293d231a6db9fec1826e6bed
STS-Engineer <administration.sts@avocarbon.com>
2026-02-11 11:17:59 +0100

    Update ocr_api.py

--- ocr_api.py
+++ ocr_api.py
@@ -60,7 +60,7 @@ def init_models():
         logging.info("Loading PaddleOCR (lang=ch)...")
         # Main OCR Engine
         paddle_engine = PaddleOCR(
-            lang="ch",
+            lang="multilingual",
             use_doc_orientation_classify=True, 
             use_doc_unwarping=False,            
             use_textline_orientation=True     
